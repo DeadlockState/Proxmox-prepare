7b3e2143f27070b66274de6875628513d69896ef
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2017-07-17 00:21:39 +0200

    Update proxmox_prepare.sh

--- proxmox_prepare.sh
+++ proxmox_prepare.sh
@@ -21,12 +21,12 @@ orange="\033[0;33m"
 red="\033[1;31m"
 nc="\033[0m"
 
-version=`pveversion | awk -v FS="(manager/| \()" '{print $2}'`
+version=`pveversion | awk -v FS="(manager/| )" '{print $2}'`
 email=`head -n 1 /etc/pve/user.cfg | grep -o -P '(?<=:::).*(?=::)'`
 fqdn=`hostname -f`
 
 echo ""
-echo " Proxmox VE "$version
+echo " Running Proxmox VE "$version
 echo ""
 
 read -p " What's your email address ? (will be used if you generate a SSL certificate) ["$email"] " new_email
@@ -42,7 +42,7 @@ echo ""
 if [ -z "$new_fqdn" ]; then
 	new_fqdn=$fqdn
 fi
-	
+
 sed -i 's/search /#search#/g' /etc/resolv.conf
 
 echo " "${blue}"Note : if you don't have any paid support subscription this script will disable \"No valid subscription\" pop-up message and will remove Proxmox VE Enterprise repository from sources.list"${nc}
@@ -57,22 +57,22 @@ if [ "$subscription" != "y" ] ; then
 	if [ -f "/usr/share/pve-manager/js/pvemanagerlib.js" ] ; then
 		sed -i 's/if (data.status !== '\''Active'\'') {/if (data.status == '\''Active'\'') {/g' /usr/share/pve-manager/js/pvemanagerlib.js
 	fi
-			
+	
 	sed -i 's/deb https/#deb #https/g' /etc/apt/sources.list.d/pve-enterprise.list
-
+	
 	apt-get update > /dev/null 2>&1
 fi
-		
+
 read -p " Do you want install Fail2ban and enable it for Proxmox (http, https and 8006 ports) ? [Y/n] " install_fail2ban
 echo ""
-		
+
 if [ "$install_fail2ban" != "n" ] ; then
 	apt-get install -y fail2ban > /dev/null 2>&1
-			
+	
 	cd /etc/fail2ban/
-			
+	
 	touch jail.local
-			
+	
 	echo "[proxmox]
 enabled = true
 port = http,https,8006
@@ -82,47 +82,93 @@ maxretry = 4
 bantime = 43200" > jail.local
 
 	cd filter.d/
-		
+	
 	touch proxmox.conf
-		
+	
 	echo "[Definition]
 failregex = pvedaemon\[.*authentication failure; rhost=<HOST> user=.* msg=.*
 ignoreregex =" > proxmox.conf
 
 	service fail2ban restart
 fi
-		
+
 if grep -qF "PermitRootLogin yes" /etc/ssh/sshd_config; then
 	read -p " SSH access by root is authorized, do you want disable it ? [y/N] " ssh_root_access
 	echo ""
-			
+	
 	if [ "$ssh_root_access" = "y" ] ; then
 		sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-				
+		
 		service ssh restart && service sshd restart
 	fi
 else
-	ssh_root_access="N"
+	ssh_root_access="n"
 fi
-		
+
 if grep -qF "Port 22" /etc/ssh/sshd_config; then
 	read -p " SSH port is default 22 do you want change it ? [y/N] " ssh_port
 	echo ""
-			
+	
 	if [ "$ssh_port" = "y" ] ; then
 		read -p " Please enter new port number for SSH : " ssh_port_number
 		echo ""
-				
+		
 		if grep -qF "#Port 22" /etc/ssh/sshd_config; then
 			sed -i 's/# Port 22/Port '$ssh_port_number'/g' /etc/ssh/sshd_config
 		else
 			sed -i 's/Port 22/Port '$ssh_port_number'/g' /etc/ssh/sshd_config
 		fi
-				
+		
 		service ssh restart && service sshd restart
 	fi
 fi
+
+read -p " Do you want add a new bridged network ? [Y/n] " add_bridged_network
+echo ""
+
+if [ "$add_bridged_network" != "n" ] ; then
+	if grep -qF "vmbr1" /etc/network/interfaces; then
+		echo " "${red}"vmbr1 already exists"${nc}
+		echo ""
+	else
+		rand_ip_network=$(shuf -i 100-200 -n 1)
 		
+		cd /root/
+
+		touch networking-up.sh networking-down.sh
+
+		echo "# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Allow traffic on vmbr1 from vmbr0
+iptables -t nat -A POSTROUTING -s '192.168."$rand_ip_network".0/24' -o vmbr0 -j MASQUERADE" >> networking-up.sh
+
+		echo "# Disable IP forwarding
+echo 0 > /proc/sys/net/ipv4/ip_forward
+
+# Close traffic on vmbr1 from vmbr0
+iptables -t nat -D POSTROUTING -s '192.168."$rand_ip_network".0/24' -o vmbr0 -j MASQUERADE" >> networking-down.sh
+		
+		chmod +x networking-*.sh
+		
+		echo "
+auto vmbr1
+iface vmbr1 inet static
+	address  192.168."$rand_ip_network".254
+	netmask  255.255.255.0
+	bridge_ports none
+	bridge_stp off
+	bridge_fd 0
+	post-up /root/networking-up.sh
+	post-down /root/networking-down.sh" >> /etc/network/interfaces
+	
+		service networking restart
+		
+		echo " "${green}"vmbr1 (192.168."$rand_ip_network".0/24) was added"${nc}
+		echo ""
+	fi
+fi
+
 read -p " Do you want generate a Let's Encrypt SSL certificate for your Proxmox server ? [Y/n] " ssl_certificate
 echo ""
 
@@ -130,21 +176,27 @@ if [ "$ssl_certificate" != "n" ] ; then
 	# Open port 80 for Let's Encrypt SSL certificate validation
 	iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 	iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
-			
+	
 	apt-get install -y git-core > /dev/null 2>&1
-			
+	
 	cd /root/
-			
+	
 	git clone https://github.com/Neilpang/acme.sh.git acme.sh-master --quiet
-			
+	
 	mkdir /etc/pve/.le
-			
+	
 	cd acme.sh-master/
-			
+	
 	./acme.sh --install --accountconf /etc/pve/.le/account.conf --accountkey /etc/pve/.le/account.key --accountemail "$new_email" > /dev/null 2>&1
-		
+	
 	./acme.sh --issue --standalone --keypath /etc/pve/local/pveproxy-ssl.key --fullchainpath /etc/pve/local/pveproxy-ssl.pem --reloadcmd "systemctl restart pveproxy" -d $new_fqdn
-			
+	
+	if [ "$?" = 0 -o "$?" = 2 ] ; then
+		acme_sh="ok"
+	else
+		acme_sh="nok"
+	fi
+	
 	# Close port 80 previously opened
 	iptables -D INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 	iptables -D OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
@@ -152,11 +204,15 @@ if [ "$ssl_certificate" != "n" ] ; then
 fi
 
 if [ "$ssl_certificate" != "n" ] ; then
-	echo " "${green}"https"${nc}"://"$new_fqdn":8006"
+	if [ "$acme_sh" = "ok" ] ; then
+		echo " "${green}"https"${nc}"://"$new_fqdn":8006"
+	else
+		echo " "${red}"https"${nc}"://"$new_fqdn":8006"
+	fi
 else
 	echo " "${red}"https"${nc}"://"$new_fqdn":8006"
 fi
-		
+
 if [ "$ssh_root_access" != "y" ] ; then
 	if [ "$ssh_port" = "y" ] ; then
 		echo " ssh -p "$ssh_port_number" root@"$new_fqdn
@@ -168,6 +224,7 @@ fi
 echo ""
 echo " For all your changes to be applied you must be restart the server"
 read -p " Do you want restart now ? [Y/n] " restart
+echo ""
 
 if [ "$restart" != "n" ] ; then
 	reboot
